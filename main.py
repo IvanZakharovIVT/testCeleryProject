2a2e974aa3cc3f65b2b194b3b790d39ff7819e1a
ivan <ivan.zakharov@nordclan.com>
2025-02-06 16:39:15 +0400

    Вынос создания пользователя в lifespan

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from config.db_config import get_session
 from config.settings import ORIGINS
+from on_start.create_default_admin import create_admin_user
 from routers.exceptions.users import setup_user_handlers
 from routers.square_task import router as task_router
 from routers.square_calculation import router as calculation_router
 from routers.users import router as user_router
 
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app1: FastAPI):
+    async for db_session in get_session():
+        await create_admin_user(db_session)
+        await db_session.flush()
+        await db_session.commit()
+    yield
 
 app = FastAPI(
     root_path="/api",  # This should match your API base URL
     openapi_url="/openapi.json",
     title="Squares calculation",
     version="1.0.0",
-    openapi_version="3.1.0"
+    openapi_version="3.1.0",
+    lifespan=lifespan
 )
 
 app.add_middleware(
--- on_start/create_default_admin.py
+++ on_start/create_default_admin.py
@@ -1,16 +1,19 @@
 from config import settings
 from database import User
 from infrastructure.enums.user_type import UserType
+from infrastructure.repository.users import UserRepository
 from infrastructure.utils.user_utils import hash_password, convert_name_to_username
 
 async def create_admin_user(db_session):
     full_name = settings.DEFAULT_ADMIN_FULL_NAME
     username = convert_name_to_username(full_name)
-    password = hash_password(settings.DEFAULT_ADMIN_PASSWORD)
-    user = User(
-        full_name=full_name,
-        username=username,
-        user_type=UserType.ADMIN.value,
-        password=password,
-    )
-    db_session.add(user)
+    existed_user = await UserRepository(db_session).get_user_by_username(username)
+    if not existed_user:
+        password = hash_password(settings.DEFAULT_ADMIN_PASSWORD)
+        user = User(
+            full_name=full_name,
+            username=username,
+            user_type=UserType.ADMIN.value,
+            password=password,
+        )
+        db_session.add(user)
