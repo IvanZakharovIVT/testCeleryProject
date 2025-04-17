ccf71fe4ebf417209f50d9a92c7e803ccae7bd23
ivan <ivan.zakharov@nordclan.com>
2025-04-17 16:23:57 +0400

    Добавление ruff

--- tests/test_user_utils/test_convert_username.py
+++ tests/test_user_utils/test_convert_username.py
@@ -1,18 +1,19 @@
 import pytest
-from src.infrastructure.services.calculate_squares import sum_of_squares
 from src.infrastructure.utils.user_utils import convert_name_to_username
 
-params_calc=(
+params_calc = (
     ("Hello There my friend 123!!!", "hello_there_my_friend_123"),
     ("_=5F3hh", "_5f3hh"),
 )
 
+
 @pytest.fixture(params=params_calc)
 def f_wrapper_function(request):
-    '''fixture: Возвращает объект'''
+    """fixture: Возвращает объект"""
     yield request.param
-    print('fixture: Завершение созданного объекта')
+    print("fixture: Завершение созданного объекта")
+
 
 def test_convert_name_to_username(f_wrapper_function):
-  calc_result = convert_name_to_username(f_wrapper_function[0])
-  assert calc_result == f_wrapper_function[1]
+    calc_result = convert_name_to_username(f_wrapper_function[0])
+    assert calc_result == f_wrapper_function[1]
